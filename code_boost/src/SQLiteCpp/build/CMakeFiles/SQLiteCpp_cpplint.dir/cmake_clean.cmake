dfc261f3434d1570093b9593d6996f966495538b
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2016-11-02 22:50:15 +0100

    - Removes unnecessary build files from repository

--- code_boost/src/SQLiteCpp/build/CMakeFiles/SQLiteCpp_cpplint.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/SQLiteCpp_cpplint"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/SQLiteCpp_cpplint.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- code_boost/src/SQLiteCpp/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/vm-exchange/SQLiteCpp/googletest
-# Build directory: /mnt/hgfs/vm-exchange/SQLiteCpp/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(googlemock)
