5f7c6c56b0d8eb26a528c6de5b0e630e0030ee44
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2016-11-03 16:48:27 +0100

    Add SQLiteCpp as a git submodule

--- code_boost/src/SQLiteCpp/build/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/vm-exchange/SQLiteCpp/googletest/googletest
-# Build directory: /mnt/hgfs/vm-exchange/SQLiteCpp/build/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
