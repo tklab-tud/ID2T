b4be8e1a84f278b8e164de45e0fa6e469516d762
aidmar.wainakh <aidmar.wainakh@gmail.com>
2017-09-07 18:42:10 +0200

    performance experiments

--- code_boost/src/cxx/pcap_processor.cpp
+++ code_boost/src/cxx/pcap_processor.cpp
@@ -121,9 +121,15 @@ void pcap_processor::collect_statistics() {
         //int timeIntervalCounter = 1;   
         //int timeIntervalsNum = 100;
         //std::chrono::microseconds intervalStartTimestamp = stats.getTimestampFirstPacket();
-        //std::chrono::microseconds firstTimestamp = stats.getTimestampFirstPacket();        
-        SnifferIterator lastpkt;
-        for (SnifferIterator j = snifferOverview.begin(); j != snifferOverview.end(); j++) {lastpkt = j;}        
+        //std::chrono::microseconds firstTimestamp = stats.getTimestampFirstPacket();   
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();    
+        SnifferIterator lastpkt; // (((((((j++)++)++)++)++)++)++)++
+        for (SnifferIterator j = snifferOverview.begin(); j != snifferOverview.end(); j++) {lastpkt = j;}  
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count()*1e-6;
+        std::cout<< "empty loop: " << duration << " sec" << std::endl;
+
+            
         //std::chrono::microseconds lastTimestamp = lastpkt->timestamp();                  
         //std::chrono::microseconds captureDuration = lastTimestamp - firstTimestamp;
         /*if(captureDuration.count()<=0){
