93b7cf0a7d7d9935a5e898d4aa722c9e48c4680a
Carlos Garcia <carlos.garcia@tk.informatik.tu-darmstadt.de>
2017-10-02 15:12:05 +0200

    Better build reliability.

--- build.sh
+++ build.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Create the Makefile using cmake, from a clean build directory
 cd code_boost/src/build/
+if [ ${PWD##*/} = 'build' ]; then
+    # Only delete everything if we are in a folder called 'build'.
+    rm -rf ./*
+else
+    echo "Error: The 'build' directory was not found."
+    exit
+fi
 cmake ..
 
 if [ -f Makefile ]; then
     make
 else
-    echo "CMake did not finish successfully."
+    echo "Error: 'cmake' did not finish successfully."
     exit
 fi
 
 if [ $? -eq 0 ]; then
     cp libpcapreader.so ../../../code/ID2TLib/
 else
-    echo "Make did not finish successfully."
+    echo "Error: 'make' did not finish successfully."
     exit
 fi
 
 cd ../../../
-#ln -s code/CLI.py id2t.py
 
 # Create the ID2T script
 cat >./id2t  <<EOF
@@ -34,5 +41,5 @@ EOF
 chmod +x ./code/CLI.py
 chmod +x ./id2t
 
-echo -e "\n\nAll is set. ID2T is ready to be used."
+echo -e "\n\nAll is set. ID2T is ready."
 echo -e "\nRun ID2T with the command './id2t'"
