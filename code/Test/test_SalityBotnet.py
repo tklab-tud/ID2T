a9bec90d7c5f37d51dee34797b30fc7abefeb7b3
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2018-03-06 12:58:07 +0100

    optimize imports in Test/test_SalityBotnet.py
    refactor whitspaces in Test/test_SalityBotnet.py

--- code/Test/test_SalityBotnet.py
+++ code/Test/test_SalityBotnet.py
@@ -1,14 +1,13 @@
 import unittest.mock as mock
 
-import Test.ID2TAttackTest as Test
 import ID2TLib.TestLibrary as Lib
+import Test.ID2TAttackTest as Test
 
 sha_botnet_basic = '72c537fba918154dbe937694d8da87260bebb05c0ad20802051fa80107c1efbe'
 sha_botnet_most_used_ip_in_list = '72c537fba918154dbe937694d8da87260bebb05c0ad20802051fa80107c1efbe'
 
 
 class UnitTestSalityBotnet(Test.ID2TAttackTest):
-
     def test_botnet_basic(self):
         self.checksum_test([['SalityBotnet']], sha_botnet_basic)
 
