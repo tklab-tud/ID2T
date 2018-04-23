d5171a9e9026874cab0a91b0863ce2cd464fe5bc
Stefan Schmidt <thrimbor@gmail.com>
2018-04-23 18:48:49 +0200

    Moved telnet reference pcap

--- code/Test/test_MembersMgmtCommAttack.py
+++ code/Test/test_MembersMgmtCommAttack.py
@@ -7,4 +7,4 @@ sha_default = '116b6cb3f1be37e50333a4f1a2535d96b1b053a4c950655391826b43585cff2b'
 class UnitTestMembersMgmtCommAttack(Test.ID2TAttackTest):
     def test_regression(self):
         self.checksum_test([['MembersMgmtCommAttack', 'hidden_mark=True']], sha_default, seed=42,
-                           pcap=Util.TEST_DIR + "Botnet/telnet-raw.pcap")
+                           pcap=Util.TEST_DIR + "reference_telnet.pcap")
