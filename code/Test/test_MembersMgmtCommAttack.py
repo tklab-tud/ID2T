7d7b55a15407c230fbfc5f47bd804945a62646e8
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2019-07-16 09:54:54 +0200

    update MembersMgmtComm test

--- code/Test/test_MembersMgmtCommAttack.py
+++ code/Test/test_MembersMgmtCommAttack.py
@@ -1,10 +1,6 @@
 import Test.ID2TAttackTest as Test
-import ID2TLib.Utility as Util
-
-sha_default = 'f57edd9fe1f8a2cf31d56f263d72d8e10c71d18cb124f0fb0b5bfcab49497419'
 
 
 class UnitTestMembersMgmtCommAttack(Test.ID2TAttackTest):
     def test_regression(self):
-        self.checksum_test([['MembersMgmtCommAttack', 'hidden_mark=True']], sha_default, seed=42,
-                           pcap=Util.TEST_DIR + "reference_telnet.pcap")
+        self.order_test([['MembersMgmtCommAttack', 'hidden_mark=True']])
