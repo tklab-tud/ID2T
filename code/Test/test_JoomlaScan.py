4536b8eeb86f795e6b18cbda88e13104c003394e
MartinBarrettNielsen11 <martindanielnielsen96@gmail.com>
2023-03-26 16:57:32 +0000

    Joomla  version scan attack
    added Joomla version scan attack.
    
    - It uses - function get_unique_random_ipv4_from_ip_network to get a unique random IPv4 address within the specified IP network of the victim server.
    - generate_ephemeral_ports() is used to generate ephemeral ports for the convo.
    
    This two features contribute to the "realness" of the attack traffic generated.

---
+++ code/Test/test_JoomlaScan.py
@@ -0,0 +1,14 @@
+import Test.ID2TAttackTest as Test
+
+class UnitTestJoomlaScan(Test.ID2TAttackTest):
+     def test_joomla_basic(self): 
+         self.order_test([['JoomlaScan']])
+
+     def test_joomla_ips_not_in_pcap(self):
+         self.order_test([['JoomlaScan', 'ip.src=12.23.211.21', 'ip.dst=2.131.44.20']])
+
+     def test_joomla_mac(self): 
+         self.order_test([['JoomlaScan','mac.src=00:0C:21:1C:60:61','mac.dst=04:0C:32:2C:63:62',]])
+
+     def test_joomla_port_source(self):
+         self.order_test([['JoomlaScan','port.src=3300']])
